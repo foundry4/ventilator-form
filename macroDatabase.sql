a50d87c3c1302ee3d2217f80648d83e19a661d1a
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-03-24 13:56:45 +0000

    add created_at

--- macroDatabase.sql
+++ macroDatabase.sql
@@ -2,5 +2,6 @@ CREATE TABLE passport_responses (
     id serial PRIMARY KEY,
     Info JSON,
     passport_number VARCHAR (200),
-    passport_expiry_date DATE
+    passport_expiry_date DATE,
+    created_at TIMESTAMP DEFAULT now()
 );
